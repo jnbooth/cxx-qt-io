0062688ebc7be9af1507f71b9a530144dcef45dd
Joshua Booth <joshua.n.booth@gmail.com>
2025-04-01 16:44:25 -0700

    remove fake headers

--- src/core/qlist/qlist.cpp
+++ src/core/qlist/qlist.cpp
@@ -1,9 +1,3 @@
-// clang-format off
-// SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
-// clang-format on
-// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
-//
-// SPDX-License-Identifier: MIT OR Apache-2.0
 #include "cxx-qt-io/qlist.h"
 
 #include <cxx-qt-lib/assertion_utils.h>
--- src/core/qpair/qpair.cpp
+++ src/core/qpair/qpair.cpp
@@ -1,9 +1,3 @@
-// clang-format off
-// SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
-// clang-format on
-// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
-//
-// SPDX-License-Identifier: MIT OR Apache-2.0
 #include "cxx-qt-io/qpair.h"
 
 #include <QtCore/QByteArray>
