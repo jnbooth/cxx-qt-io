7d4dddcd678585744428a01da42fa0043a93e23f
Joshua Booth <joshua.n.booth@gmail.com>
2025-07-06 14:27:19 -0700

    fix pre-commit script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
+# shellcheck disable=SC2086
 
 CHANGED_RUST_FILES="$(git diff --name-only --staged --diff-filter=AM -- '*.rs')"
 if [ -n "$CHANGED_RUST_FILES" ]; then
-  rustfmt +nightly --check "$CHANGED_RUST_FILES"
+  rustfmt +nightly --check $CHANGED_RUST_FILES
 fi
 
 CHANGED_SHELL_FILES="$(git diff --name-only --staged --diff-filter=AM -- '*.sh')"
 if [ -n "$CHANGED_SHELL_FILES" ]; then
-  shellcheck "$CHANGED_SHELL_FILES"
+  shellcheck $CHANGED_SHELL_FILES
 fi
 
 cargo clippy --all-targets --all-features -- -D warnings
