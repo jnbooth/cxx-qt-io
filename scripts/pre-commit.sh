2debea82a860cf5f73c904fad02ceb8dbc781636
Joshua Booth <joshua.n.booth@gmail.com>
2025-07-12 13:40:27 -0700

    run git-clang-format first

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -6,10 +6,11 @@ if [ -n "$CHANGED_RUST_FILES" ]; then
   rustfmt +nightly --check $CHANGED_RUST_FILES
 fi
 
+git clang-format -f
+
 CHANGED_SHELL_FILES="$(git diff --name-only --staged --diff-filter=AM -- '*.sh')"
 if [ -n "$CHANGED_SHELL_FILES" ]; then
   shellcheck $CHANGED_SHELL_FILES
 fi
 
 cargo clippy --all-targets --all-features -- -D warnings
-git clang-format -f
