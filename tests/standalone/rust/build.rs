ed0ba81f315d49bb29812013e824a5a3a983d6aa
Joshua Booth <joshua.n.booth@gmail.com>
2026-01-26 16:29:45 -0800

    fix buildscript on Windows

--- tests/standalone/rust/build.rs
+++ tests/standalone/rust/build.rs
@@ -1,17 +1,12 @@
-use std::fs;
-
 use cxx_qt_build::CxxQtBuilder;
 
 fn main() {
     CxxQtBuilder::new()
-        .files(fs::read_dir("src").unwrap().filter_map(|file| {
-            let path = file.ok()?.path();
-            let s = path.to_str()?;
-            if s.starts_with("src/q") && s.ends_with(".rs") {
-                Some(path)
-            } else {
-                None
-            }
-        }))
+        .files([
+            "src/qbuffer.rs",
+            "src/qdeadlinetimer.rs",
+            "src/qdtlsgeneratorparameters.rs",
+            "src/qfile.rs",
+        ])
         .build();
 }
